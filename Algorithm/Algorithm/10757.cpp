99dfe1fb292cca6de781a146deceee166ce4901f
hsj321 <hsjmail0603@gmail.com>
2021-01-28 23:11:36 +0900

    visual studio 변경사항 커밋

--- Algorithm/Algorithm/10757.cpp
+++ Algorithm/Algorithm/10757.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
 
 int main(void) {
-	unsigned _int64 a, b;
+	string a, b;
 	cin >> a >> b;
-	cout << a + b;
+
+	int aLen = a.size();
+	int bLen = b.size();
+	bool ten = false;
+	string answer(max(aLen, bLen), '0');
+	for (int i = 0; i < max(aLen, bLen); i++) {
+
+	}
 }
