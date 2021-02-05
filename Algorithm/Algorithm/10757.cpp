44a57894f979659873a2459db7205531dd780696
hsj321 <hsjmail0603@gmail.com>
2021-02-05 18:20:09 +0900

    백준 10757번 풀이

--- Algorithm/Algorithm/10757.cpp
+++ Algorithm/Algorithm/10757.cpp
@@ -4,16 +4,46 @@
 
 using namespace std;
 
-
 int main(void) {
 	string a, b;
+	string answer;
+	int ten = false;
 	cin >> a >> b;
-
 	int aLen = a.size();
 	int bLen = b.size();
-	bool ten = false;
-	string answer(max(aLen, bLen), '0');
-	for (int i = 0; i < max(aLen, bLen); i++) {
-		
+
+	reverse(a.begin(), a.end());
+	reverse(b.begin(), b.end());
+
+	
+	if (aLen < bLen) {
+		for (int i = 0; i < bLen - aLen; i++) {
+			a.push_back('0');
+		}
 	}
+	else if (bLen < aLen){
+		for (int i = 0; i < aLen - bLen; i++) {
+			b.push_back('0');
+		}
+	}
+
+	cout << a << endl;
+	cout << b << endl;
+	
+	
+	for (int i = 0; i < max(aLen, bLen); i++) {
+		int temp = a[i] + b[i] - '0' - '0' + ten;
+		if (temp >= 10) {
+			ten = true;
+			temp -= 10;
+		}
+		else
+			ten = false;
+		answer.push_back(temp + '0');
+	}
+	if (ten) {
+		answer.push_back('1');
+	}
+	reverse(answer.begin(), answer.end());
+	cout << answer;
 }
